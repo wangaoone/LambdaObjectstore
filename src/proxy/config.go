70df302adfc4a4f7313685c70efb2b7bc3312e3a
Ao Wang <awang24@gmu.edu>
2020-01-08 21:38:57 -0500

    update lambda warmup timeout

--- src/proxy/config.go
+++ src/proxy/config.go
@@ -10,7 +10,7 @@ const LambdaMaxDeployments = 400
 const NumLambdaClusters = 400
 const LambdaStoreName = "LambdaStore"
 const LambdaPrefix = "Proxy2Node"
-const InstanceWarmTimout = 10 * time.Minute
+const InstanceWarmTimout = 1 * time.Minute
 
 func init() {
 	lambdastore.WarmTimout = InstanceWarmTimout
