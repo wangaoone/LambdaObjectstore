701935c9a007006a21d9c8a19b91dcf684141487
Scusemua <benrcarver@gmail.com>
2020-11-25 12:04:48 -0500

    Updated proxy list in config.go

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -70,4 +70,4 @@ const ActiveReplica = 2 //min
 // If running on one proxy, then can be left empty. But for multiple, build static proxy list here
 // of private ip addr. and port.
 //var ProxyList []string
-var ProxyList []string = []string{"10.0.119.246:6378", "10.0.113.107:6378"}
+var ProxyList []string = make([]string, 0) // []string{"10.0.119.246:6378", "10.0.113.107:6378"}
