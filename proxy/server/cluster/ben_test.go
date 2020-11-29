5b56af0ee04b7eedcbed8c203823ca475ad985d2
Scusemua <benrcarver@gmail.com>
2020-11-29 15:38:23 -0500

    Working on unit tests for consistent hash ring

---
+++ proxy/server/cluster/ben_test.go
@@ -0,0 +1,42 @@
+package cluster
+
+import (
+	"github.com/buraksezer/consistent"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type clientMember string
+
+func (m clientMember) String() string {
+	return string(m)
+}
+
+var _ = Describe("ConsistentHashRing", func() {
+	It("Should return the same proxy for the given key.", func() {
+		cfg := consistent.Config{
+			PartitionCount:    271,
+			ReplicationFactor: 20,
+			Load:              1.25,
+			Hasher:            hasher{},
+		}
+		members := make([]consistent.Member, len(addrArr))
+
+		members[0] = clientMember("10.0.109.88:6378")
+		members[1] = clientMember("10.0.109.89:6378")
+
+		c.Ring = consistent.New(members, cfg)
+
+		key = "mr.srt-res-0"
+
+		//member1 := c.Ring.LocateKey([]byte(key))
+		//host1 := member1.String()
+
+		//member2 := c.Ring.LocateKey([]byte(key))
+		//host2 := member2.String()
+
+		for i := 0; i < 10; i++ {
+			Expect(c.Ring.LocateKey([]byte(key)).String()).To(Equal("10.0.109.88:6378"))
+		}
+	})
+})
