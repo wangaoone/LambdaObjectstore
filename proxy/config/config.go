686a5d89dec93d992b71d9f50add27a208a06df6
Scusemua <benrcarver@gmail.com>
2020-12-02 21:55:35 -0500

    Updated configuration again.

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -32,10 +32,10 @@ const InstanceDegradeWarmTimeout = 5 * time.Minute
 
 // InstanceCapacity Capacity of deployed Lambda functions.
 // TODO: Detectable on invocation. Can be specified by option -funcap for now.
-const DefaultInstanceCapacity = 3008 * 1000000 // MB
+const DefaultInstanceCapacity = 1024 * 1000000 // 1GB
 
 // InstanceOverhead Memory reserved for running program on Lambda functions.
-const InstanceOverhead = 100 * 1000000 // MB
+const InstanceOverhead = 300 * 1000000 // 300 MB
 
 // Threshold Scaling out avg instance size threshold
 const Threshold = 0.8 // Don't set beyond 0.8
